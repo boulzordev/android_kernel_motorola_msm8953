7e93f2d2008d92e70cb5311398b1b6d3f729c424
Pranav Vashi <neobuddy89@gmail.com>
2018-02-26 23:33:40 +0530

    cpufreq: cpu-boost: Prevent boost when device is suspended
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    [aviraxp: convert to display state API]

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -23,6 +23,7 @@
 #include <linux/slab.h>
 #include <linux/input.h>
 #include <linux/time.h>
+#include <linux/display_state.h>
 
 struct cpu_sync {
 	int cpu;
@@ -237,6 +238,9 @@ static void cpuboost_input_event(struct input_handle *handle,
 		unsigned int type, unsigned int code, int value)
 {
 	u64 now;
+
+	if (!is_display_on())
+		return;
 
 	if (!input_boost_enabled)
 		return;
